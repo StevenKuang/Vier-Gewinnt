add9814291a8554d103a4f7e3bff897e3b801606
StevenKuang <441334176@qq.com>
2018-11-28 17:58:28 +0100

    Update Aufgabe5.py
    The match function is completed, and I used \t in print_matrix function instead of 3 spaces

--- Aufgabe5.py
+++ Aufgabe5.py
@@ -18,29 +18,53 @@ __email__ = "limingkuang@gmail.com, s9108655@stud.uni-frankfurt.de"
 
 def print_matrix(lst):
     """A simple function to print a 2 dimentional list elegantly. """
-    print('\n'.join(['   '.join([str(i) for i in row]) for row in lst]))
+    print('\n'.join(['\t'.join([str(i) for i in row]) for row in lst]))
 
 
-def match(board):
+def match(b):
     """match is a function to determine if any one of the players has won the game.
     It takes the baord and the location of the last played disk as parameters.
+    b for board, val for value
     """
     flag = False
-    for i in range(9): 
-        for j in range(10): 
-            color = board[i][j]
-
-    if board[i][j+1] != color and board[i][j-1] != color:
-        if (board[i+1][j] == color and board[i+1][j+1] == color and board[i+2][j+1] == color) or
-        ((board[i+1][j] == color and board[i+1][j-1] == color and board[i+2][j-1] == color)):
-            return 1
-        else:
-            return 0
-    else:
-
+    for i in range(8): 
+        for j in range(9): 
+            val = b[i][j]
+            # a = b[i][j+1] == val and b[i-1][j+1] == val and b[i-1][j+2] == val
+            # b = b[i][j+1] == val and b[i+1][j+1] == val and b[i+1][j+2] == val
+            # c = b[i+1][j] == val and b[i+1][j+1] == val and b[i+2][j+1] == val
+            # d = b[i+1][j] == val and b[i+1][j-1] == val and b[i+2][j-1] == val
+            if val == None:
+                pass
+            else:
+                if i == 0:
+                    if j == 8:
+                        if b[i+1][j] == val and b[i+1][j+1] == val and b[i+2][j+1] == val:
+                            flag = True
+                    else:
+                        if ((b[i][j+1] == val and b[i+1][j+1] == val and b[i+1][j+2] == val) or
+                        (b[i+1][j] == val and b[i+1][j+1] == val and b[i+2][j+1] == val)):
+                            flag = True
+                elif i == 7:
+                    if j == 8:
+                        pass
+                    else:
+                        if ((b[i][j+1] == val and b[i-1][j+1] == val and b[i-1][j+2] == val) or 
+                        (b[i][j+1] == val and b[i+1][j+1] == val and b[i+1][j+2] == val)):
+                            flag = True
+                elif j == 8:
+                    if ((b[i+1][j] == val and b[i+1][j+1] == val and b[i+2][j+1] == val) or 
+                    (b[i+1][j] == val and b[i+1][j-1] == val and b[i+2][j-1] == val)):
+                        flag = True
+                else:
+                    if ((b[i][j+1] == val and b[i-1][j+1] == val and b[i-1][j+2] == val) or 
+                    (b[i][j+1] == val and b[i+1][j+1] == val and b[i+1][j+2] == val) or 
+                    (b[i+1][j] == val and b[i+1][j+1] == val and b[i+2][j+1] == val) or 
+                    (b[i+1][j] == val and b[i+1][j-1] == val and b[i+2][j-1] == val)):
+                        flag = True
+    return flag
         
             
-
 def main():
     """Here to start this module from the console or shell. """
     board=[]            # create an empty 9*10 chess board
